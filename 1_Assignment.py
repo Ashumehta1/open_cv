15be781ee5a344143858b6145c02a0a30d01e3ef
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-08-24 13:12:48 +0530

    1_assignment

---
+++ 1_Assignment.py
@@ -0,0 +1,13 @@
+#load image-->convert into gray scale--> ask window name from user to show-->ask file name --> save
+import cv2
+img=cv2.imread("my_pic.png")
+if img is not None:
+    img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    window_name=input("Enter window name: ")
+    cv2.imshow(window_name,img_gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    file_name=input("Enter file name to save: ")
+    cv2.imwrite(file_name,img_gray)
+else:
+    print("Error: Img not find")
