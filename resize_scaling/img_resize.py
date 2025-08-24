fdb25bf40f2ec374230d8a69e45c52da3a48487a
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-08-24 14:40:29 +0530

    resized commited

--- resize_scaling/img_resize.py
+++ resize_scaling/img_resize.py
@@ -1,7 +1,8 @@
 import cv2
 img=cv2.imread("D:\project\myPproject\Open_cv\krishna.jpg")
+
 if img is not None:
-    img_resize=cv2.resize(img,(100,100))
+    img_resize=cv2.resize(img,(500,600))
     cv2.imshow("original",img)
     cv2.imshow("resized",img_resize)
     cv2.waitKey(0)
