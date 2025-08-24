4f23500da4d0dad21df303869f1287e6f02ec7bf
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-08-24 14:36:44 +0530

    resized_and_scaling

---
+++ resize_scaling/img_resize.py
@@ -0,0 +1,10 @@
+import cv2
+img=cv2.imread("D:\project\myPproject\Open_cv\krishna.jpg")
+if img is not None:
+    img_resize=cv2.resize(img,(100,100))
+    cv2.imshow("original",img)
+    cv2.imshow("resized",img_resize)
+    cv2.waitKey(0)
+    cv2.destroyedAllwindows()
+else:
+    print("Error: image coul not loaded")
