0279842c616f02374ecddf30adc906e5f55f7a25
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-08-30 11:37:01 +0530

    corrected video capture

--- video/video_saving.py
+++ video/video_saving.py
@@ -3,14 +3,14 @@ camera=cv2.VideoCapture(0)#use same camera of laptop
 frame_width=int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height=int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 codec=cv2.VideoWriter_fourcc(*'mp4v')
-record=cv2.VideoWriter("myvideo.mp4",codec,0,(frame_width,frame_height))
+record=cv2.VideoWriter("myvideo.mp4",codec,20,(frame_width,frame_height))
 while True:
     success, image=camera.read()
     if not success:
         break
     record.write(image)
     cv2.imshow("live",image)
-    if cv2.waitKey(0) & 0xFF==ord('q'):
+    if cv2.waitKey(1) & 0xFF==ord('q'):#waitKey(0) was capturing only one frame
         break
 camera.release()
 record.release()
